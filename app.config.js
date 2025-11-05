f6ed76852d56db19f866403a14ed42c44bd1ba38
Adriano Marinho <adriano.marinho@FLEXPRINT.local>
2025-11-05 15:22:44 -0300

    feat: Corrige exibição de imagens de perfil e produtos

--- app.config.js
+++ app.config.js
@@ -3,11 +3,41 @@ import 'dotenv/config';
 
 export default ({ config }) => ({
   ...config,
+  name: "TrazAi Loja",
+  slug: "trazai-loja",
+
   extra: {
     apiBaseUrl: process.env.API_BASE_URL,
     assetBaseUrl: process.env.ASSET_BASE_URL,
     eas: {
       projectId: '09852f0c-9a00-455b-bbfc-c7de37bedf91'
     }
-  }
+  },
+
+  plugins: [
+    ...(config.plugins || []),
+    [
+      "expo-notifications",
+      {
+        icon: "./assets/images/notification-icon.png",
+        color: "#DC2626", // cor do ícone Android
+        sounds: [
+          "./assets/notification-sound.wav"
+        ]
+      }
+    ],
+    "@react-native-firebase/app",
+  ],
+
+  android: {
+    ...config.android,
+    package: "com.adriano_marinho.trazailojista",
+    googleServicesFile: "./android/app/google-services.json",
+    permissions: [
+      "android.permission.RECEIVE_BOOT_COMPLETED",
+      "android.permission.VIBRATE",
+      "android.permission.POST_NOTIFICATIONS"
+    ],
+    useNextNotificationsApi: true
+  },
 });
