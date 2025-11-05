a0f3c958b41ca626fb506a948ac1f927274f8619
Adriano Marinho <adriano.marinho@FLEXPRINT.local>
2025-11-05 13:08:49 -0300

    feat: Corrige exibição de imagens de perfil e produtos

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,16 +1,11 @@
-// app/(auth)/_layout.tsx
 import React from 'react';
 import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
   return (
-    <Stack>
-      {/* Aqui as rotas de autenticação.
-          'index' será a rota padrão deste grupo (o Login, que moveremos para cá).
-          'register' será a rota /register */}
-      <Stack.Screen name="index" options={{ headerShown: false }} /> {/* Tela de Login - nome real: app/(auth)/index.tsx */}
-      <Stack.Screen name="register" options={{ title: 'Cadastro de Loja' }} />
-      {/* Você pode ajustar options={{ headerShown: false }} aqui se não quiser o cabeçalho padrão */}
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="register" />
     </Stack>
   );
 }
