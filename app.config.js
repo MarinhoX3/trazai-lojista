c56b432723911ae8699cb57c7fa691861c5f328d
Adriano Marinho <adriano.marinho@FLEXPRINT.local>
2025-11-25 10:31:52 -0300

    feat: Corrige exibição de imagens de perfil e produtos

--- app.config.js
+++ app.config.js
@@ -2,19 +2,16 @@ import 'dotenv/config';
 
 export default ({ config }) => ({
   ...config,
+
   name: "TrazAi Loja",
   slug: "traz-ai-lojista",
 
   extra: {
-    apiBaseUrl: process.env.API_BASE_URL,
-    assetBaseUrl: process.env.ASSET_BASE_URL,
-    eas: {
-      projectId: '09852f0c-9a00-455b-bbfc-c7de37bedf91'
-    }
+    ...config.extra, // 👈 mantém tudo que está no app.json
   },
 
   notification: {
-    icon: "./assets/images/notification-icon.png", // 👈 força o uso do ícone branco
+    icon: "./assets/images/notification-icon.png",
     color: "#0B7709",
     androidMode: "default",
     androidCollapsedTitle: "TrazAí Loja"
@@ -34,13 +31,12 @@ export default ({ config }) => ({
 
   android: {
     ...config.android,
-    package: "com.adriano_marinho.trazailojista",
     googleServicesFile: "./google-services.json",
     useNextNotificationsApi: true,
     permissions: [
       "android.permission.RECEIVE_BOOT_COMPLETED",
       "android.permission.VIBRATE",
       "android.permission.POST_NOTIFICATIONS"
-    ],
-  },
+    ]
+  }
 });
