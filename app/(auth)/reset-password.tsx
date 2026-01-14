47bf49223814b7daa058dcc359a39cd58c204d8a
adriano_marinho <marinhox3@hotmail.com>
2026-01-14 15:27:17 -0300

    fix: add expo-clipboard

--- app/(auth)/reset-password.tsx
+++ app/(auth)/reset-password.tsx
@@ -43,7 +43,7 @@ export default function ResetPasswordScreen() {
       });
 
       Alert.alert("Sucesso", "Senha alterada com sucesso!");
-      router.replace("/(auth)/login");
+      router.replace("/(auth)/index");
 
     } catch (err: any) {
       console.log("ERRO API:", err?.response?.data || err);
